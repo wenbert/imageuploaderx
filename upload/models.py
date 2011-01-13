c66a113a2dcf621a525c70431f67ac4a896819e0
wenbert <wenbert@ubuntu-wenbert.(none)>
2011-01-13 06:13:55 +0100

    removed blank fields

--- upload/models.py
+++ upload/models.py
@@ -16,16 +16,16 @@ class Uploads(models.Model):
     size          = models.FloatField(verbose_name='Size')
     bandwidth     = models.FloatField(verbose_name='Bandwidth Used')
     source        = models.URLField(verbose_name='URL Source')
-    filehash      = models.CharField(unique=True, max_length=250, verbose_name='Unique hash for the file')
+    filehash      = models.CharField(max_length=250, verbose_name='Unique hash for the file')
     
     def __unicode__(self):
         return "%s.%s" % (self.uuid,self.ext)
         
     def view_image(self):
-        return "%s/%s.%s" % (settings.SITE_URL,self.uuid,self.ext)
+        return "%s.%s" % (settings.SITE_URL,self.uuid,self.ext)
 
 class UploadsAdmin(admin.ModelAdmin):
-    list_display = ('view_image', 'ext', 'views', 'ip', 'uploaded_on','size', 'bandwidth','filehash', 'source')
+    list_display = ('view_image', 'ext', 'views', 'ip', 'uploaded_on','size', 'bandwidth', 'source')
     #list_filter = ('ip', 'source')
     search_fields = ('filename', 'uuid', 'ip', 'source', 'filehash')
 
@@ -35,7 +35,7 @@ def delete_upload(sender, **kwargs):
     is the same as the name of the group that was created.
     """
     instance = kwargs['instance']
-    path_to_delete = '%s/%s%s' % (instance.path,instance.uuid,instance.ext)
+    path_to_delete = '%s/%s.%s' % (instance.path,instance.uuid,instance.ext)
     if not os.path.isdir(path_to_delete):
         os.unlink(path_to_delete)
         
